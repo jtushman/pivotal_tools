04c0b17c8b6de7c3027e50b4bd2f2882be0de179
Jonathan Tushman <jtushman@pipewave.com>
2013-07-31 14:58:50 -0400

    initial dump

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pivotal_readme',
+      version='0.1',
+      description='Simple Tool to generate a README for your upcoming release',
+      url='http://github.com/jtushman/pivotal_readme',
+      author='Jonathan Tushman',
+      author_email='jonathan.tushman@zefr.com',
+      license='MIT',
+      packages=['pivotal_readme'],
+      zip_safe=False)
